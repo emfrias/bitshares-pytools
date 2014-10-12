04f3389e331e6d701663f63433030664e07d290b
Fabian Schuh <mail@xeroc.org>
2014-10-12 13:34:02 +0200

    print floating amount of funds

--- bots/market_maker.py
+++ bots/market_maker.py
@@ -15,24 +15,25 @@ class MarketMaker():
 
     def execute(self):
         self.log.info("Executing bot:  %s" % self.name)
-        tolerance = self.config["external_price_tolerance"]
-        spread = self.config["spread_percent"]
-        min_balance = self.config["min_balance"]
+        tolerance      = self.config["external_price_tolerance"]
+        spread         = self.config["spread_percent"]
+        min_balance    = self.config["min_balance"]
         min_order_size = self.config["min_order_size"]
-        quote = self.quote_symbol
-        base = self.base_symbol
+        quote          = self.quote_symbol
+        base           = self.base_symbol
 
         # FIXME add weights
         true_price_per_btsx = statistics.median(self.exchanges.price_inbtsx[self.quote_symbol])
-        median = self.client.get_median(self.quote_symbol)
-        new_price_per_btsx = true_price_per_btsx
+        median              = self.client.get_median(self.quote_symbol)
+        new_price_per_btsx  = true_price_per_btsx
 
         if median > new_price_per_btsx:
             new_price_per_btsx = median * MEDIAN_EDGE_MULTIPLE
 
+        ## Cancel open orders if necessary
         canceled = []
-        quote_freed = 0
-        base_freed = 0
+        quote_freed = 0 # freed funds from closing open orders
+        base_freed  = 0
         
         result = self.client.get_asks_out_of_range(self.name, quote, base, new_price_per_btsx * (1+spread), tolerance)
         canceled.extend( result[0] )
@@ -46,9 +47,9 @@ class MarketMaker():
         canceled.extend( result[0] )
         quote_freed += result[1]
 
-        asset_balance = self.client.get_balance(self.name, quote) + quote_freed
-        btsx_balance = self.client.get_balance(self.name, base) + base_freed
-        available_btsx_balance = btsx_balance - min_balance
+        asset_balance                = self.client.get_balance(self.name, quote) + quote_freed
+        btsx_balance                 = self.client.get_balance(self.name, base) + base_freed
+        available_btsx_balance       = btsx_balance - min_balance
         available_asset_buy_quantity = (asset_balance / new_price_per_btsx) - min_balance;
         
         new_orders = []
@@ -56,14 +57,14 @@ class MarketMaker():
         if available_asset_buy_quantity > min_order_size:
             self.log.info("Submitting a bid...")
             new_orders.append(["bid_order", [self.name, available_asset_buy_quantity, base, new_price_per_btsx, self.quote_symbol]])
-        else:
-            self.log.info("Skipping bid - %s balance of %d is too low" % (self.quote_symbol, available_asset_buy_quantity))
+        else :
+            self.log.info("Skipping bid - %s balance of %.5f is too low" % (self.quote_symbol, available_asset_buy_quantity))
 
         if available_btsx_balance > min_order_size:
             self.log.info("submitting an ask...")
             new_orders.append(["ask_order", [self.name, available_btsx_balance, base, new_price_per_btsx * (1+spread), self.quote_symbol]])
-        else:
-            self.log.info("Skipping ask - %s balance of %d is too low" % (self.base_symbol, available_btsx_balance))
+        else :
+            self.log.info("Skipping ask - %s balance of %.5f is too low" % (self.base_symbol, available_btsx_balance))
 
         if len(canceled) > 0 or len(new_orders) > 0:
             self.log.info("Committing orders.")
