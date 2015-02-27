924697e688d21845c8d7322aca20766b0ac8f020
Fabian Schuh <mail@xeroc.org>
2015-02-27 20:57:45 +0100

    more work

--- papersharedrop/pil.py
+++ papersharedrop/pil.py
@@ -1,17 +1,23 @@
 from PIL import Image
 from PIL import ImageFont, ImageDraw
 import qrcode
- 
+import csv
 
-img = Image.open('02dfed9e_l.png', 'r')
-img_w, img_h = img.size
- 
-draw = ImageDraw.Draw(img)
-draw.text((10, 15), "Hello")
-draw.text((10, 25), "world")
+with open('wallet.csv', 'r') as csvfile:
+    spamwriter = csv.reader(csvfile, delimiter=';')
+    cnt = 0
+    for wif,add in spamwriter :
+        cnt+=1
+        img = Image.open('02dfed9e_l.png', 'r')
+        img_w, img_h = img.size
+        
+        draw = ImageDraw.Draw(img)
+        # draw.textsize(wif)
+        #draw.text((228, 445), wif, fill=( 0,0,0,0 ))
+        draw.text((228, 445), add, fill=( 0,0,0,0 ))
+        draw.text((761, 26), "1 USD", fill=( 0,0,0,0 ))
+        img.paste( qrcode.make(wif).resize((180,180)), (998, 27) )
+        img.paste( qrcode.make(add).resize((180,180)), (25, 306) )
 
-qr = qrcode.make('Some data here')
-
-img.paste( qr, (20, 30) )
-img.show()
-
+        #img.show()
+        img.save("paperwallet-%03d.png"%cnt, "PNG")
--- papersharedrop/sharedrop-manual.py
+++ papersharedrop/sharedrop-manual.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python2
 import binascii
-import Transactions
 from pprint import pprint
 import json
-import bitsharesrpc
 import config
+import bitsharestools.address as Address
+import bitsharestools.transactions as Transaction
+import bitsharesrpc
 
 withdrawals = [
             {
@@ -55,23 +56,23 @@ ops      = []
 privkeys = []
 ### Withdraw Fee in BTS!
 for w in withdrawals :
-    withdraw = Transactions.Withdraw( w["balance_id"], w["amount"])
-    ops.append(Transactions.Operation( "withdraw_op_type", withdraw ))
+    withdraw = Transaction.Withdraw( w["balance_id"], w["amount"])
+    ops.append(Transaction.Operation( "withdraw_op_type", withdraw ))
     privkeys.append(w["privkey"])
 
 for d in receiveAddresses :
-    wst      = Transactions.WithdrawSignatureType( d[ "address" ] )
-    wc       = Transactions.WithdrawCondition( d["asset_id"], slate_id, "withdraw_signature_type", wst)
-    deposit  = Transactions.Deposit( d["amount"], wc )
-    ops.append(Transactions.Operation( "deposit_op_type", deposit))
+    wst      = Transaction.WithdrawSignatureType( d[ "address" ] )
+    wc       = Transaction.WithdrawCondition( d["asset_id"], slate_id, "withdraw_signature_type", wst)
+    deposit  = Transaction.Deposit( d["amount"], wc )
+    ops.append(Transaction.Operation( "deposit_op_type", deposit))
 
-tx       = Transactions.Transaction( 60*60*12, 0, ops )
-sigtx    = Transactions.SignedTransaction(tx, privkeys)
+tx       = Transaction.Transaction( 60*60*12, 0, ops )
+sigtx    = Transaction.SignedTransaction(tx, privkeys)
 
 print( json.dumps( sigtx.tojson(  ), sort_keys=True, indent=4 ) )
 print(binascii.hexlify( sigtx.towire(  ) ))
 
 ## Send operation
-print "="*80
-rpc = bitsharesrpc.client(config.url, config.user, config.passwd)
-print(rpc.blockchain_broadcast_transaction(sigtx.tojson()))
+#print "="*80
+#rpc = bitsharesrpc.client(config.url, config.user, config.passwd)
+#print(rpc.blockchain_broadcast_transaction(sigtx.tojson()))
