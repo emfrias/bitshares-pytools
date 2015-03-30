e2c633036f50fe6d0587d6759987afcd7df3f45e
Fabian Schuh <schuh@lnt.de>
2015-03-30 13:50:32 +0200

    sharedrop update

--- papersharedrop/README.md
+++ papersharedrop/README.md
@@ -1,2 +1,26 @@
-## Download Font ##
-* http://www.fontsquirrel.com/fonts/lato
+# What is this #
+
+This tool will sharedrop an arbitrary amount amount/symbol pairs into an
+(almost) arbitrary amount of address/privkey pair.
+You can use this to preload paperwallets for meetups and conferences.
+
+# Howto use #
+
+* Open your BitShares client
+* Get a new address to use for funding the wallets (say `BTS-address`) with
+    
+    wallet_address_create <account>
+* Send the funds you want to distribute to that address (the client will figure
+  out the private key for that address by asking the client)
+* Define the RPC connection in `config.py` (see example in `config-example.py`)
+* Run the sharedrop script like this
+
+    python2 sharedrop.py <address_with_funds> <number_of_wallets> "<amount-asset-pair>" ["<amount-asset-pair>"]
+
+# Examples #
+
+    python2 sharedrop.py BTSPFjktNqSUadcATtCzMm3zn1LmQystnX3B 10 "1 NOTE"
+
+    python2 sharedrop.py BTSPFjktNqSUadcATtCzMm3zn1LmQystnX3B 10 "1 USD" "0.5 BTS"
+
+    python2 sharedrop.py BTSPFjktNqSUadcATtCzMm3zn1LmQystnX3B 5 "1 USD" "1 NOTE" "1 BTS"
--- papersharedrop/sharedrop.py
+++ papersharedrop/sharedrop.py
@@ -7,6 +7,8 @@ import bitsharestools.address as Address
 import bitsharestools.transactions as Transaction
 import bitsharesrpc
 import csv
+import argparse
+import binascii
 
 ################################################################################
 def query_yes_no(question, default="yes"):
@@ -32,66 +34,148 @@ def query_yes_no(question, default="yes"):
             sys.stdout.write("Please respond with 'yes' or 'no' "
                              "(or 'y' or 'n').\n")
 ################################################################################
-btsprecision       = 1e5  
 
-print( "Connecting to BitShares RPC" )
-rpc = bitsharesrpc.client(config.url, config.user, config.passwd)
-rpc.wallet_open(config.wallet)
-rpc.unlock(999999, config.unlock)
+def main() : 
+    parser = argparse.ArgumentParser(description='Construct paperwallets according to file')
+    parser.add_argument('fromaddress', type=str, help='Address to take funds from')
+    parser.add_argument('number', type=int, help='Number of sharedrop addresses to create')
+    parser.add_argument('fundwith', nargs='+', type=str, help='Fund to send to each address')
+    parser.add_argument('--displayprecision', type=str, help='Display precision for paper wallet')
+    parser.add_argument('--txfee', type=str, help='Transaction fee')
+    parser.add_argument('--rpcurl', type=str, help='')
+    parser.add_argument('--rpcuser', type=str, help='')
+    parser.add_argument('--rpcpasswd', type=str, help='')
+    parser.add_argument('--wallet', type=str, help='')
+    parser.add_argument('--unlock', type=str, help='')
+    parser.add_argument('--filename', type=str, help='')
+    parser.set_defaults(rpcurl=config.url, rpcuser=config.user, rpcpasswd=config.passwd, wallet=config.wallet, unlock=config.unlock, txfee=config.txfee, displayprecision=2, filename="sharedrop-signed-transaction.txt")
+    args = parser.parse_args()
 
-### Withdraw Fee in BTS!
-privkeys = []
-ops      = []
-for bID in config.balanceids :
-    print( "Reading BalanceID: %s" % bID )
-    balance = rpc.blockchain_get_balance(bID)[ "result" ]
+    ''' Connect to bitshares client via RPC '''
+    rpc = bitsharesrpc.client(args.rpcurl, args.rpcuser, args.rpcpasswd)
+    rpc.wallet_open(args.wallet)
+    rpc.unlock(999999, args.unlock)
 
-    print( " - Constructing Withdraw operation" )
-    if balance["condition"]["asset_id"] is 0 :
-        withdraw = Transaction.Withdraw(bID, config.txfee*btsprecision)
-    else :
-        sharedropamount    = balance["balance"]
-        sharedropsymbol    = rpc.blockchain_get_asset(balance["condition"]["asset_id"])["result"]["symbol"]
-        sharedropprecision = rpc.blockchain_get_asset(balance["condition"]["asset_id"])["result"]["precision"]
-        sharedropassetid   = balance["condition"]["asset_id"]
-        withdraw = Transaction.Withdraw(bID, sharedropamount)
-    ops.append(Transaction.Operation("withdraw_op_type", withdraw ))
-    owner = balance["condition"]["data"]["owner"]
-    print( " - Getting private key for signature of owner %s" % owner)
-    privkeys.append(rpc.wallet_dump_private_key(owner)["result"])
+    ###
+    print("Assets for sharedrop into each address:")
+    fundwith = {}
+    sharedroptext = ""
+    for fund in args.fundwith :
+        try :
+            amount,symbol = str(fund).split(" ")
+            amount = float(amount)
+            asset = rpc.blockchain_get_asset(symbol)['result']
+            assert asset is not None, "Asset %s does not exist on the blockchain" % symbol
+        except :
+            print("Something went wrong when loading 'fundwith'.")
+            parser.print_usage()
+            raise
+        fundwith[ symbol ] = amount*asset["precision"]
+        sharedroptext += "%.*f%s " % (args.displayprecision, amount, symbol)
+        print(" + %f %s" % (amount, symbol))
 
-print "Sharedropping %f %s" % ( sharedropamount/float(sharedropprecision), sharedropsymbol )
-amount = sharedropamount/float(sharedropprecision)/float(config.numberpaperwallets)
-print "- each %f %s" % ( amount, sharedropsymbol )
+    ### Withdraw Fee in BTS!
+    have     = {}
+    print("Getting balance from given address")
+    balanceids = rpc.blockchain_list_address_balances(args.fromaddress)['result']
+    for bIDobj in balanceids :
+        bID       = bIDobj[0]
+        balance   = bIDobj[1]
+        if symbol == "BTS" : ## Substract TX fee right away
+            balance["balance"] -= int(args.txfee * 1e5)
+        asset_id  = balance["condition"]["asset_id"]
+        asset     = rpc.blockchain_get_asset(asset_id)["result"]
+        precision = float(asset["precision"])
+        amount    = int(balance["balance"])/precision
+        symbol    = asset["symbol"]
+        have[symbol] = [amount, bIDobj, asset]
 
-print( "Generating %d new addresses for sharedrop" % config.numberpaperwallets )
-wifs = []
-for didx in xrange( 1, config.numberpaperwallets ) :
-    wif     = Address.newwif()
-    wifs.append(wif)
-    address  = Address.wif2btsaddr(wif)
-    print( " - Constructing deposit operation for address %s" % address )
-    wst      = Transaction.WithdrawSignatureType( address )
-    wc       = Transaction.WithdrawCondition( sharedropassetid, None, "withdraw_signature_type", wst)
-    deposit  = Transaction.Deposit( amount, wc )
-    ops.append(Transaction.Operation( "deposit_op_type", deposit))
+    ''' Check if enough funded '''
+    for symbol in fundwith.keys() : 
+        if symbol in have :
+            if fundwith[ symbol ] * args.number > balance["balance"] :
+                raise Exception("Not enough funds. Need %f %s, but only have %f %s" %(fundwith[ symbol ]*args.number/precision, symbol, amount, symbol))
+        else : 
+            raise Exception("Missing asset %s in address." % symbol)
 
-print( "Finalizing Transaction" )
-tx       = Transaction.Transaction( 60*60*12, 0, ops )
-print( "Signing Transaction" )
-sigtx    = Transaction.SignedTransaction(tx, privkeys)
+    privkeys = []
+    ops      = []
+    print( "Constructing Withdraw operations" )
+    for symbol in fundwith.keys() :
+        bIDobj    = have[symbol][1]
+        asset     = have[symbol][2]
+        bID       = bIDobj[0]
+        balance   = bIDobj[1]
+        asset_id  = asset["id"]
+        precision = float(asset["precision"])
+        amount    = fundwith[symbol] * args.number
+        if symbol == "BTS" :
+            amount += args.txfee * 1e5
+        ops.append(Transaction.Operation("withdraw_op_type", Transaction.Withdraw(bID, amount) ))
+        owner = balance["condition"]["data"]["owner"]
+        privkeys.append(rpc.wallet_dump_private_key(owner)["result"])
 
-print( "Storing WIF and ADDRESS in wallet.csv backup" )
-with open('wallet.csv', 'wb') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=';')
-    for wif in wifs :
-        spamwriter.writerow([wif, Address.wif2btsaddr(wif), "%f %s" %(amount,sharedropsymbol), sharedropsymbol])
+    ''' Ensure that TX fee is paied '''
+    if "BTS" not in fundwith : 
+        symbol = "BTS"
+        print( "Enforcing TX fee" )
+        if symbol in have :
+            if have[symbol] < args.txfee * 1e5 :
+                raise Exception("Not enough funds. Need %f %s, but only have %f %s" %( args.txfee, symbol, have[symbol], symbol))
+        else : 
+            raise Exception("Missing asset %s in address." % symbol)
+        bIDobj    = have[symbol][1]
+        asset     = have[symbol][2]
+        bID       = bIDobj[0]
+        balance   = bIDobj[1]
+        asset_id  = asset["id"]
+        precision = float(asset["precision"])
+        amount    = args.txfee * precision
+        ops.append(Transaction.Operation("withdraw_op_type", Transaction.Withdraw(bID, amount) ))
+        owner = balance["condition"]["data"]["owner"]
+        print( " - Getting private key for signature of owner %s" % owner)
+        privkeys.append(rpc.wallet_dump_private_key(owner)["result"])
 
-## Send operation
-print( "Broadcasting transaction to the blockchain" )
-print( json.dumps( sigtx.tojson(), indent=4 ) )
-if query_yes_no( "Please confirm the transaction" ) :
-    print( "Transmitting!!" )
-    print(rpc.blockchain_broadcast_transaction(sigtx.tojson()))
-else :
-    print("blockchain_broadcast_transaction " + str(sigtx.tojson()).replace(' ',''))
+    print( "Constructing Deposit operations" )
+    wifs = []
+    for didx in xrange( 0, args.number ) :
+        wif     = Address.newwif()
+        wifs.append(wif)
+        address  = Address.wif2btsaddr(wif)
+        for symbol in fundwith.keys() :
+            bIDobj    = have[symbol][1]
+            asset     = have[symbol][2]
+            asset_id  = asset["id"]
+            amount    = fundwith[symbol]
+            wst      = Transaction.WithdrawSignatureType( address )
+            wc       = Transaction.WithdrawCondition( asset_id, None, "withdraw_signature_type", wst)
+            deposit  = Transaction.Deposit( amount, wc )
+            ops.append(Transaction.Operation( "deposit_op_type", deposit))
+
+    print( "Finalizing Transaction" )
+    tx       = Transaction.Transaction( 60*60*12, 0, ops )
+
+    print( "Signing Transaction" )
+    sigtx    = Transaction.SignedTransaction(tx, privkeys)
+
+    print( "Storing WIF and ADDRESS in wallet.csv backup" )
+    with open('wallet.csv', 'wb') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=';')
+        for wif in wifs :
+            spamwriter.writerow([wif, Address.wif2btsaddr(wif), "%s" %sharedroptext, ""])
+
+    # Send operation
+    print( json.dumps( sigtx.tojson(), indent=4 ) )
+    with open(args.filename,'wb') as fp :
+        fp.write(json.dumps(sigtx.tojson()))
+    print("Transaction stored in %s" % args.filename)
+
+    if query_yes_no( "Please confirm the transaction" ) :
+        print( "Broadcasting transaction to the blockchain" )
+        print(rpc.blockchain_broadcast_transaction(sigtx.tojson()))
+    else : 
+        print("Not broadcasted. Signed transaction stored in file %s" % args.filename)
+    rpc.lock()
+
+if __name__ == '__main__':
+    main()
