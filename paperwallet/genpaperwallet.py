cbee7c736d9e065f91ecffcc84ae52c1bfb549cd
Fabian Schuh <schuh@lnt.de>
2015-03-31 11:08:44 +0200

    pimping paperwallets for @clains

--- paperwallet/genpaperwallet.py
+++ paperwallet/genpaperwallet.py
@@ -13,7 +13,12 @@ def main() :
     parser.add_argument('--asset', type=str, help='Asset label to be placed on the paper wallet')
     parser.add_argument('--design', type=str, help='Design of the paperwallet (defaults to "cass")')
     parser.add_argument('-encrypt', help='Encrypt private key with BIP38!', action='store_true')
+    parser.add_argument('--back_qr', help='Content of the QR-code on the back of the paper wallet', action='store_true')
+    parser.add_argument('--back_text1', help='Text field 1 on the back of the paper wallet', action='store_true')
+    parser.add_argument('--back_text2', help='Text field 2 on the back of the paper wallet', action='store_true')
+    parser.add_argument('--back_text3', help='Text field 3 on the back of the paper wallet', action='store_true')
     parser.set_defaults(design="cass")
+    parser.set_defaults(back_text1="Install the BitShares App.", back_text2="Load your funds from the wallet", back_text3="Buy the BitShares 101 Book for $1", back_qr="http://cryptofresh.com/products/8?r=xeroc")
     args = parser.parse_args()
 
     ''' Generate new wif and address '''
@@ -34,7 +39,7 @@ def main() :
             else :
                 print("Given Passphrases do not match!")
 
-    front,back = Paper.paperwallet(wif, add, args.amount, args.asset, encrypt=pw, design=args.design)
+    front,back = Paper.paperwallet(wif, add, args.amount, args.asset, encrypt=pw, design=args.design, backtext1=args.back_text1, backtext2=args.back_text2, backtext3=args.back_text3, backQRcode=args.back_qr)
 
     extension = os.path.splitext(args.filename)[1]
     if extension == ".svg" :
