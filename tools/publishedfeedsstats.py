1508903f7d99c5149da0da98be4c4c477a4896d8
Fabian Schuh <mail@xeroc.org>
2015-05-05 17:19:54 +0200

    delegate-slate 0.9 compatible

--- tools/publishedfeedsstats.py
+++ tools/publishedfeedsstats.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
    feedtime = datetime.strptime(feed["last_update"],"%Y-%m-%dT%H:%M:%S")
    delta    = (currenttime-feedtime)
    if feed[ "asset_symbol" ] not in feedprice : feedprice[ feed["asset_symbol"] ] = [  ]
-   feedprice[ feed["asset_symbol"] ].append(feed[ "price" ])
+   feedprice[ feed["asset_symbol"] ].append(float(feed[ "price" ]))
    if delta.total_seconds()/60/60/24 < 1.0 :
     validfeed.append(feed["asset_symbol"])
     validfeeds.append( feed );
@@ -54,9 +54,9 @@ if __name__ == "__main__":
  for p in delegatefeeds : 
   assetstr = ""
   for a in p[ "feeds" ] :
-   deviation_from_median = (a[ "price" ]-medianPrice[ a["asset_symbol"]])/medianPrice[ a["asset_symbol"]] * 100
+   deviation_from_median = (float(a["price"]) - medianPrice[ a["asset_symbol"]])/medianPrice[ a["asset_symbol"]] * 100
    if deviation_from_median > 1.5 :
-    assetstr += "%8s, %11.8f (med%+8.3f%%)\n" % (a["asset_symbol"], a[ "price" ], deviation_from_median)
+    assetstr += "%8s, %11.8f (med%+8.3f%%)\n" % (a["asset_symbol"], float(a[ "price" ]), deviation_from_median)
   if assetstr != "" :
    tableLargeDev.add_row([p["name"], p["top"], p["numValidFeeds"], assetstr ])
  print("\n\n\nLarge deviation Feeds")
@@ -68,7 +68,7 @@ if __name__ == "__main__":
  tableAll.border                  = True
  for p in delegatefeeds : 
   assetstr = ""
-  assetstr = "\n".join([ "%8s, %11.8f (med%+8.3f%%)" % (a["asset_symbol"], a[ "price" ], 100*(a[ "price" ]-medianPrice[ a[ "asset_symbol" ]])/medianPrice[ a[ "asset_symbol" ]]) for a in p[ "feeds" ] ])
+  assetstr = "\n".join([ "%8s, %11.8f (med%+8.3f%%)" % (a["asset_symbol"],float(a["price"]), 100*(float(a["price"])-medianPrice[ a[ "asset_symbol" ]])/medianPrice[ a[ "asset_symbol" ]]) for a in p[ "feeds" ] ])
   tableAll.add_row([p["name"], p["top"], p["numValidFeeds"], assetstr ])
  print("\n\n\nAll deviation")
  print(tableAll.get_string(sortby="top", reversesort=False))
